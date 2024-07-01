69a4eb0eebcad460a6890abf1de5a7250931af25
sameerpatel2161983 <sameerpatel2161983@gmail.com>
2024-07-01 10:00:21 +0000

    start

---
+++ project/app/page.js
@@ -0,0 +1,11 @@
+import "./style.css"
+export default function Home() {
+  return (
+    <>
+    <div className="flower"><img src="flowers.png" alt="" /></div>
+    <div className="main">
+      You are Invited on Wedding of Lorem. and Lorem.
+    </div>
+    </>
+  );
+}
